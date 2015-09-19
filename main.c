8f773ed211c537e0da33cb2a78efd7c93c442337
patch7 <patch_m@mail.ru>
2015-09-19 12:29:55 +0300

    добавил функцию тактирования порта Е, добавил функцию инициализации пина 8 порте Е на вывод(для светодиода), добавил функцию для мигания светодиода с задержкой, В функции main добавил вызов функций(Init: RCC,GPIO), создал задачу vLedTask, запустил шедулер.

--- main.c
+++ main.c
@@ -8,16 +8,48 @@
 #include "task.h"
 #include "queue.h"
 
-void main()
+void vFreeRTOSInitRCC()
 {
-  ;
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
+}
+
+void vFreeRTOSInitGPIO()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+}
+
+void vLedTask(void *pvParameters)
+{
+	while(1)
+	{
+		GPIOE->ODR ^= GPIO_Pin_8;
+		vTaskDelay(50);
+	}
+  vTaskDelete(NULL);
+}
+
+int main()
+{
+  vFreeRTOSInitRCC();
+  vFreeRTOSInitGPIO();
+  xTaskCreate(vLedTask, "vLedTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+  vTaskStartScheduler();
+  return 0;
 }
 
 void vApplicationIdleHook(void)
 {}
 
 void vApplicationMallocFailedHook(void)
-{}
+{
+	for(;;);
+}
 
 void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName)
 {
